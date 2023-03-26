be556812a344816a5f1ad692c8b4e64ae7a92c58
DDeviak <volodybs@gmail.com>
2023-03-26 21:13:33 +0300

    Reimplemented Polish

--- Polish/Polish/Polish.cpp
+++ Polish/Polish/Polish.cpp
@@ -1,125 +1,140 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
 #include <iostream>
 #include <cmath>
 #include <string>
 #include <sstream>
+#include <stack>
 
 using namespace std;
 
-class MathTree
-{
-public:
-	MathTree() {}
-	MathTree(string exp) 
+size_t IsOperation(char c) {
+	switch (c)
 	{
-		root_ = new Node(exp);
+	case '+':
+		return 1;
+		break;
+	case '-':
+		return 2;
+		break;
+	case '*':
+		return 3;
+		break;
+	case '/':
+		return 4;
+		break;
+	case '^':
+		return 5;
+		break;
+	default:
+		return 0;
+		break;
 	}
-	~MathTree() {}
-
-	void Output(ostream &out) {
-		root_->Output(out);
-	}
-	double Calculate() {
-		root_->Calculate();
-		return root_->value;
-	}
-
-private:
-	struct Node
+}
+string ToPolish(string exp) {
+	stack<char> stack;
+	string output;
+	for (size_t i = 0; i < exp.size(); i++)
 	{
-		char operation = 0;
-		double value;
-		Node* left = nullptr;
-		Node* right = nullptr;
-
-		Node() :operation(0) {}
-		Node(string exp) {
-			size_t i, b = 0;
-			size_t l = exp.length();
-			if (exp[0] == '(')
+		if (exp[i] == ' ')continue;
+		if (exp[i] == '(')stack.push(exp[i]);
+		else if (exp[i] == ')') 
+		{
+			while (stack.top()!='(')
 			{
-				for (i = 0; i < l; i++)
-				{
-					if (exp[i] == '(') b++;
-					if (exp[i] == ')') b--;
-					if (b == 0)
-					{
-						i++;
-						break;
-					}
-				}
+				output += stack.top();
+				output += " ";
+				stack.pop();
 			}
-			else 
-			{
-				i = 0;
-				while ((exp[i] <= '9' && exp[i] >= '0') || exp[i] == '.')
-				{
-					i++;
-					if (i == l) break;
-				}
-			}
-
-			if (i == l) 
-			{
-				stringstream ss;
-				ss << exp;
-				ss >> value;
-			}
-			else {
-				operation = exp[i];
-				left = new Node(exp.substr(exp[0] == '(', i - (exp[0] == '(')*2));
-				right = new Node(exp.substr(i + 1 + (exp[i + 1] == '('), l - i - 1 - (exp[l - 1] == ')')));
-			}
+			stack.pop();
 		}
-
-		void Output(ostream& out) {
-			if (left != nullptr && right != nullptr) {
-				left->Output(out);
-				right->Output(out);
-				out << operation;
-			}
-			else
+		else if (IsOperation(exp[i])) {
+			while (!stack.empty() && (IsOperation(exp[i]) < IsOperation(stack.top())))
 			{
-				out << value;
+				output += stack.top();
+				output += " ";
+				stack.pop();
 			}
-			out << " ";
+			stack.push(exp[i]);
 		}
-		void Calculate() {
-			if (left != nullptr && right != nullptr) {
-				left->Calculate();
-				right->Calculate();
+		else
+		{
+			while (exp[i] <= '9' && exp[i] >= '0')
+			{
+				output += exp[i];
+				i++;
 			}
-			switch (operation)
+			output += " ";
+		}
+	}
+	while (!stack.empty())
+	{
+		if (stack.top() != '(') {
+			output += stack.top();
+			output += " ";
+		}
+		stack.pop();
+	}
+	output.pop_back();
+	return output;
+}
+double Calculate(string exp) {
+	stack<double> stack;
+	string t;
+	for (size_t i = 0; i < exp.size(); i++)
+	{
+		if (exp[i] == ' ') {
+			if (t != "") {
+				stack.push(stod(t));
+				t = "";
+			}
+			continue;
+		}
+		else if (IsOperation(exp[i]))
+		{
+			double a, b;
+			b = stack.top();
+			stack.pop();
+			a = stack.top();
+			stack.pop();
+			switch (exp[i])
 			{
 			case '+':
-				value = left->value + right->value;
+				stack.push(a + b);
 				break;
 			case '-':
-				value = left->value - right->value;
+				stack.push(a - b);
 				break;
 			case '*':
-				value = left->value * right->value;
+				stack.push(a * b);
 				break;
 			case '/':
-				value = left->value / right->value;
+				stack.push(a / b);
 				break;
 			case '^':
-				value = pow(left->value, right->value);
+				stack.push(pow(a, b));
 				break;
 			default:
+				throw "Invalid operation";
 				break;
 			}
 		}
-	};
+		else t += exp[i];
+	}
+	return stack.top();
+}
 
-	Node* root_ = nullptr;
+TEST_CASE("Testing Polish notation") {
+	
+	CHECK(ToPolish("2 + 3 * 4") == "2 3 4 * +");
+	CHECK(Calculate("2 3 4 * +") == 14);
 
-};
+	CHECK(ToPolish("(2 + 3) * 4") == "2 3 + 4 *");
+	CHECK(Calculate("2 3 + 4 *") == 20);
 
-int main()
-{
-	string s;
-	cin >> s;
-	MathTree mt(s);
-	mt.Output(cout);
-	cout << " = " << mt.Calculate();
+	CHECK(ToPolish("2 ^ 3 + 4") == "2 3 ^ 4 +");
+	CHECK(Calculate("2 3 ^ 4 +") == 12);
+
+	CHECK(ToPolish("2 + 3 * 4 - 5") == "2 3 4 * + 5 -");
+	CHECK(Calculate("2 3 4 * + 5 -") == 9);
 }
