2e2273952dffd112f0cac1a120da4f2dd4413544
DDeviak <volodybs@gmail.com>
2023-04-11 16:48:20 +0300

    Fixed some bugs in Matrix Added Unit-tests

--- Matrix/Matrix/Matrix.cpp
+++ Matrix/Matrix/Matrix.cpp
@@ -1,12 +1,15 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <iostream>
+#include "D:\ASD\doctest.h"
 #include <list>
+#include <stdexcept>
 
 using namespace std;
 
 class Matrix
 {
 public:
-	Matrix(int** arr, size_t n = 0, size_t m = 0) :n_(n), m_(m) {
+	Matrix(int** arr = nullptr, size_t n = 0, size_t m = 0) :n_(n), m_(m) {
 		for (size_t i = 0; i < n_; i++)
 		{
 			rows_.push_back(pair<size_t, list<pair<size_t, int>>>(i, list<pair<size_t, int>>()));
@@ -24,7 +27,7 @@ public:
 	~Matrix() {}
 
 	void SetElement(size_t i, size_t j, int el) {
-		if (!(i < n_ && j < m_))throw "Index out of bounds";
+		if (!(i < n_ && j < m_))throw out_of_range("Index out of bounds");
 
 		list<pair<size_t, int>>* row = nullptr;
 		list<pair<size_t, list<pair<size_t, int>>>>::iterator it_i;
@@ -71,7 +74,7 @@ public:
 	}
 	
 	int GetElement(size_t i, size_t j) {
-		if (!(i < n_ && j < m_))throw "Index out of bounds";
+		if (!(i < n_ && j < m_))throw out_of_range("Index out of bounds");
 		
 		list<pair<size_t, int>>* row = nullptr;
 		list<pair<size_t, list<pair<size_t, int>>>>::iterator it_i;
@@ -123,7 +126,7 @@ public:
 	}
 
 	Matrix* Add(Matrix* m) {
-		if ((n_ == m->n_) && (m_ == m->m_)) throw "Unable to add Matrixes with different size";
+		if (!((n_ == m->n_) && (m_ == m->m_))) throw invalid_argument("Unable to add Matrixes with different size");
 		
 		int** arr = m->ToArr();
 
@@ -157,37 +160,142 @@ public:
 	size_t GetColumnsCount() { return m_; }
 
 	void SetRowsCount(size_t n) { 
+		if (n < n_) {
+			rows_.remove_if([&](pair<size_t, list<pair<size_t, int>>> t) { return t.first >= n; });
+		}
 		n_ = n;
-		list<pair<size_t, list<pair<size_t, int>>>>::iterator it_i;
-		for (it_i = rows_.begin(); it_i != rows_.end(); it_i++)
-		{
-			if (it_i->first >= n) {
-				rows_.erase(it_i);
-			}
-		}
 	}	
-	void SetColumnsCount(size_t m) { 
-		m_ = m;
-		list<pair<size_t, list<pair<size_t, int>>>>::iterator it_i;
-		list<pair<size_t, int>>::iterator it_j;
-		for (it_i = rows_.begin(); it_i != rows_.end(); it_i++)
-		{
-			for (it_j = it_i->second.begin(); it_j != it_i->second.end(); it_j++)
+	void SetColumnsCount(size_t m) {
+		if (m < m_) {
+			list<pair<size_t, list<pair<size_t, int>>>>::iterator it_i;
+			for (it_i = rows_.begin(); it_i != rows_.end(); it_i++)
 			{
-				if (it_i->first >= m) {
-					it_i->second.erase(it_j);
-				}
+				it_i->second.remove_if([&](pair<size_t, int> t) { return t.first >= m; });
 			}
 		}
+		m_ = m;
 	}
 
+	size_t ActualSize() {
+		size_t size = 0;
+		list<pair<size_t, list<pair<size_t, int>>>>::iterator it_i;
+		for (it_i = rows_.begin(); it_i != rows_.end(); it_i++)
+		{
+			size += it_i->second.size();
+		}
+		return size;
+	}
 private:
 	size_t n_, m_;
 	list<pair<size_t, list<pair<size_t, int>>>> rows_;
 };
 
 
-int main()
-{
-    
+TEST_CASE("Testing Matrix") {
+	int arr[4][5] = {
+		{0,2,0,6,5},
+		{0,0,0,0,0},
+		{0,9,5,4,0},
+		{1,0,0,0,0}
+	};
+	//Testing default constructor
+	Matrix m;
+	CHECK(m.GetRowsCount() == 0);
+	CHECK(m.GetColumnsCount() == 0);
+
+	//Testing size setting
+	m.SetRowsCount(4);
+	m.SetColumnsCount(5);
+
+	CHECK(m.GetRowsCount() == 4);
+	CHECK(m.GetColumnsCount() == 5);
+
+	m.Output(cout);
+	cout << endl;
+
+	//Filling matrix
+	//Testing get/set methods
+	size_t n = 0;
+	for (size_t i = 0; i < 4; i++)
+	{
+		for (size_t j = 0; j < 5; j++)
+		{
+			CHECK(m.GetElement(i, j) == 0);
+			m.SetElement(i, j, arr[i][j]);
+			CHECK(m.GetElement(i, j) == arr[i][j]);
+			n += (size_t)(arr[i][j] != 0);
+		}
+	}
+
+	//Testing memory usage
+	CHECK(m.ActualSize() == n);
+	
+	m.Output(cout);
+	cout << endl;
+
+	//Testing Index out of range behaviour
+	CHECK_THROWS_AS(m.GetElement(6, 6), out_of_range);
+	CHECK_THROWS_AS(m.SetElement(6, 6, 6), out_of_range);
+	
+	//Testing dynamic size increase
+	m.SetRowsCount(7);
+	m.SetColumnsCount(7);
+	CHECK_NOTHROW(m.GetElement(6, 6));
+	CHECK_NOTHROW(m.SetElement(6, 6, 6));
+	
+	m.Output(cout);
+	cout << endl;
+
+	//Testing memory usage
+	CHECK(m.ActualSize() == ++n);
+
+	//Testing dynamic size decrease
+	m.SetRowsCount(3);
+	m.SetColumnsCount(3);
+	CHECK_THROWS_AS(m.GetElement(3, 3), out_of_range);
+	CHECK_THROWS_AS(m.SetElement(3, 3, 6), out_of_range);
+	
+	//Testing memory usage
+	CHECK(m.ActualSize() < n);
+
+	m.Output(cout);
+	cout << endl;
+
+	int** arr1 = new int*[4];
+	for (size_t i = 0; i < 4; i++)
+	{
+		arr1[i] = new int[4];
+		for (size_t j = 0; j < 4; j++)
+		{
+			arr1[i][j] = -arr[i][j];
+		}
+	}
+
+	//Testing constructor with parameters
+	Matrix m1(arr1, 4, 4);
+	m1.Output(cout);
+	cout << endl;
+
+	//Testing Addition method with diferent matrixes sizes
+	CHECK_THROWS_AS(m.Add(&m1), invalid_argument);
+
+	m1.SetRowsCount(3);
+	m1.SetColumnsCount(3);
+
+	//Testing Addition method with proper arguments
+	Matrix* t = nullptr;
+	CHECK_NOTHROW(t = m.Add(&m1));
+	t->Output(cout);
+	cout << endl;
+
+	//Testing memory usage
+	CHECK(t->ActualSize() == 0);
+
+	for (size_t i = 0; i < 4; i++)
+	{
+		delete[] arr1[i];
+	}
+	delete[] arr1;
+
+	delete t;
 }
